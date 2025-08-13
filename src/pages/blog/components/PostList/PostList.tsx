38d4dce9d5f06b1f80b740bf745d903ecc75bfb8
Tuandev18 <phamtuan180704@gmail.com>
2025-08-13 21:16:06 +0700

    fetch api rtk-query

--- src/pages/blog/components/PostList/PostList.tsx
+++ src/pages/blog/components/PostList/PostList.tsx
@@ -1,6 +1,14 @@
+import { useGetPostsQuery } from 'pages/blog/blog.service'
 import PostItem from '../PostItem'
+import { Fragment } from 'react'
+import SkeletonPost from 'pages/blog/components/SkeletonPost'
+
 
 export default function PostList() {
+  //isLoading chỉ dành cho lần fetch đầu tiền
+  //isFetching là cho mỗi lần gọi API
+   const {data,isLoading,isFetching} = useGetPostsQuery()
+   console.log(data,isLoading,isFetching)
   return (
     <div className='bg-white py-6 sm:py-8 lg:py-12'>
       <div className='mx-auto max-w-screen-xl px-4 md:px-8'>
@@ -11,10 +19,19 @@ export default function PostList() {
           </p>
         </div>
         <div className='grid gap-4 sm:grid-cols-2 md:gap-6 lg:grid-cols-2 xl:grid-cols-2 xl:gap-8'>
+          {isFetching && (
+            <Fragment>
+              <SkeletonPost/>
+              <SkeletonPost/>
+            </Fragment>
+          )}
+        {!isFetching && data?.map((post)=>
+          <PostItem key={post.id} post={post}/>    
+        )}
+          {/* <PostItem />
           <PostItem />
           <PostItem />
-          <PostItem />
-          <PostItem />
+          <PostItem /> */}
         </div>
       </div>
     </div>
